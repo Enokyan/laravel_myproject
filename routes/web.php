d1af903646c645708b2190bd80aba50453f9f495
Enokyan <jon.enokyan@mail.ru>
2017-01-11 10:04:11 +0400

    Jon commit

---
+++ routes/web.php
@@ -0,0 +1,92 @@
+<?php
+
+//Auth
+Route::get('{locale}/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('{locale}/login', 'Auth\LoginController@login');
+Route::post('{locale}/logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('{locale}/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('{locale}/register', 'Auth\RegisterController@register');
+Route::get('{locale}/home', 'HomeController@index');
+
+
+//socialit
+Route::get('/redirect/{provider?}', 'SocialAuthController@redirect');
+Route::get('/callback/{provider?}', 'SocialAuthController@callback');
+
+// update delete
+Route::get('{locale}/delete', ['uses' => 'UsersController@delete']);
+Route::get('{locale}/update', ['uses' => 'UsersController@update']);
+
+//chat//
+Route::get('{locale}/send_chat', 'ChatController@index');
+Route::post('chat/add', 'ChatController@store');
+
+Route::get('ajax', 'ChatController@ajax');
+Route::post('upload_sms', ['uses' => 'ChatController@upload']);
+
+
+//select two chat
+Route::get('send_sms_pdf', ['uses' => 'MailController@sendChatMessage'])->middleware('login_auth');
+Route::get('{locale}/send_chat_two', ['uses' => 'ChatTwoController@index'])->middleware('login_auth');
+Route::post('select_twochat', ['uses' => 'ChatTwoController@select_twochat']);
+Route::post('chat2/add2', 'ChatTwoController@store');
+Route::get('ajax2', 'ChatTwoController@select_new');
+Route::get('ajax3', 'ChatTwoController@select_count_name');
+Route::post('/delete_msg', 'ChatTwoController@delete_msg');
+Route::post('/updatemessage', 'ChatTwoController@updatemessage');
+Route::post('/getupdatedmessages', 'ChatTwoController@getupdatedmessages');
+
+
+
+
+//online friend
+Route::get('online_friend', 'ChatTwoController@online_friend');
+Route::get('logaut_online', 'ChatTwoController@logaut_online');
+
+
+//users select chat
+Route::post('users/select', ['uses' => 'ChatTwoController@users_select']);
+
+//users
+Route::get('/', ['uses' => 'UsersController@select_all']);
+Route::post('product', ['as' => 'product.index', 'uses' => 'ProductController@create']);
+Route::get('user', ['uses' => 'UsersController@select']);
+Route::get('email', ['uses' => 'UsersController@index']);
+Route::post('sendmail', ['as' => 'sendmail.index', 'uses' => 'UsermailController@sendmail']);
+
+//send request friend
+Route::post('frendrequest', ['uses' => 'UsersController@frendrequest']);
+Route::post('/searchusers', ['uses' => 'HomeController@searchUsers']);
+
+//getfriendrequests
+Route::post('/getfriendrequests', ['uses' => 'UsersController@getfriendrequests']);
+Route::post('/selectfriendrequests', ['uses' => 'UsersController@selectfriendrequests']);
+Route::post('/addfriend', ['uses' => 'UsersController@addfriend']);
+Route::post('/deletefriend', ['uses' => 'UsersController@deletefriend']);
+
+
+
+
+
+//fullcalendar
+
+
+Route::get('{locale}/events', ['uses' => 'FullCalendarController@showcalendar']);
+Route::get('calendar_result', ['uses' => 'FullCalendarController@index']);
+
+////// update user
+Route::post('/user/update', ['uses' => 'HomeController@update_user']);
+
+
+
+//Admin
+Route::get('/admin', 'AdminController@loginForm');
+Route::post('checkdata', 'AdminController@checkdata');
+Route::get('admin_home', 'AdminController@home');
+Route::get('admin_logout', 'AdminController@adminlogout');
+Route::get('deleteuser/{id}', 'AdminController@deleteuser');
+Route::get('users', 'AdminController@users');
+Route::post('updateuserdata', 'AdminController@updateuserdata');
+Route::get('viewprofile/{id}', 'AdminController@viewprofile');
